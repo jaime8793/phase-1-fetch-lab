6bbdef88ca5a425e1114b9ad277b57443663dd18
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-08-28 11:04:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -8,7 +10,7 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
-    main.appendChild(h2);
+    main.append(h2);
   });
 }
 
